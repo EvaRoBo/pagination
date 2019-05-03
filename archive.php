0432792b8893aad86f5c8a3a920d1a105f31dda6
Eva Rohony-Bodnar <39854036+EvaRoBo@users.noreply.github.com>
2019-05-03 22:05:13 +0100

    Create archive.php

---
+++ archive.php
@@ -0,0 +1,8 @@
+<?php the_posts_pagination(); ?>
+
+
+<?php the_posts_pagination( array(
+	'mid_size' => 2,
+	'prev_text' => __( 'Back', 'textdomain' ),
+	'next_text' => __( 'Onward', 'textdomain' ),
+) ); ?>
